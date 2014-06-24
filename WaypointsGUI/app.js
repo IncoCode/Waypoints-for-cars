bd2f1e5792b561e309998cd67e95bc088506491b
IncoCode <akulich.dimon@gmail.com>
2014-06-24 21:46:55 +0300

    some buttons are working

--- WaypointsGUI/app.js
+++ WaypointsGUI/app.js
@@ -1,31 +1,30 @@
 function WaypointsGUI(){}
 
 WaypointsGUI.prototype.initialize = function(){
-    //this._updateHydrosData();
-    var widget = this;
+    //var widget = this;
 	
-	$('<button>Start recording path</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
+	$('<button>Start recording path</button>').appendTo(this.rootElement).click(function(){	
+        callSystemLuaFunction("wayPoints.startRecordingPath", "");
     });
 	
 	$('<button>Stop recording path</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
-    });
+        callSystemLuaFunction("wayPoints.stopRecordingPath", "");
+    });	
 	
 	$('<button>Save waypoints</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
+        //beamng.callSystemLuaFunction("wayPoints.");
     });
 	
 	$('<button>Load waypoints</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
+        //beamng.callSystemLuaFunction("beamstate.breakAllBreakgroups()");
     });
 	
 	$('<button>Run all cars</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
+        callSystemLuaFunction("wayPoints.runAllCars", "");
     });
 	
 	$('<button>Print current car id</button>').appendTo(this.rootElement).click(function(){
-        beamng.sendActiveObjectLua("beamstate.breakAllBreakgroups()");
+        callSystemLuaFunction("wayPoints.printCurrentCarId", "");
     });
 };
 
