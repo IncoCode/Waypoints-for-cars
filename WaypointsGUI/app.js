4f8432383a214c59ce67730b198ecb29ea957044
IncoCode <akulich.dimon@gmail.com>
2014-06-25 16:55:35 +0300

    save waypoints

--- WaypointsGUI/app.js
+++ WaypointsGUI/app.js
@@ -1,7 +1,11 @@
 function WaypointsGUI(){}
 
-WaypointsGUI.prototype.initialize = function(){
-    //var widget = this;
+function isInt(n) {
+   return typeof n === 'number' && n % 1 == 0;
+}
+
+WaypointsGUI.prototype.initialize = function(){	
+	$('<div id="popup_box" hidden><a id="popupBoxClose">Close</a></div>').appendTo(this.rootElement);	
 	
 	$('<button>Start recording path</button>').appendTo(this.rootElement).click(function(){	
         callSystemLuaFunction("wayPoints.startRecordingPath", "");
@@ -9,10 +13,25 @@ WaypointsGUI.prototype.initialize = function(){
 	
 	$('<button>Stop recording path</button>').appendTo(this.rootElement).click(function(){
         callSystemLuaFunction("wayPoints.stopRecordingPath", "");
-    });	
+    });
 	
 	$('<button>Save waypoints</button>').appendTo(this.rootElement).click(function(){
-        //beamng.callSystemLuaFunction("wayPoints.");
+		callSystemLuaFuncCallback("wayPoints.getCarsId()", function(arg){
+			console.log(arg);
+			$.each(arg, function(index, value) {
+				if (index == 'selected') {
+					return true;
+				}
+				var option = $("<option></option>")
+					.attr("value", index)
+					.text(index);
+				if (arg['selected'] == index) {
+					option.attr("selected", "1");
+				}
+				$('#carIds').append(option);
+			});			
+		});
+		$('#popup_box').fadeIn("slow");
     });
 	
 	$('<button>Load waypoints</button>').appendTo(this.rootElement).click(function(){
@@ -26,6 +45,27 @@ WaypointsGUI.prototype.initialize = function(){
 	$('<button>Print current car id</button>').appendTo(this.rootElement).click(function(){
         callSystemLuaFunction("wayPoints.printCurrentCarId", "");
     });
+	
+	$('#popup_box').load("apps/WaypointsGUI/saveForm.html", function() {
+	
+		$("#saveWayPoints").click(function() {
+			console.log("4");
+			var carId = $("#carIds option:selected").val();
+			var fileName = $("#fileName").val();
+			callSystemLuaFunction("wayPoints.saveWayPoints", carId +",\"" +fileName +"\"");
+			$('#popup_box').fadeOut("slow");
+			$('#fileName').val("");
+			$('#carIds')
+				.find('option')
+				.remove();
+		});
+		
+		$("#closeSaveForm").click(function(){
+			$('#popup_box').fadeOut("slow");
+		});
+	});
+	
+	console.log("WaypointsGUI inizialize");
 };
 
 WaypointsGUI.prototype.update = function(streams) {
