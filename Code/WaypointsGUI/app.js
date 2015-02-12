61b82564b78b1227d7cec25145525bc9e3183162
IncoCode <akulich.dimon@gmail.com>
2015-02-12 17:17:24 +0200

    added buttons for stop and run car commands (fixed #3)

--- Code/WaypointsGUI/app.js
+++ Code/WaypointsGUI/app.js
@@ -15,6 +15,39 @@ WaypointsGUI.prototype.fillCarsId = function(comboBox) {
 	});
 }
 
+WaypointsGUI.prototype.stopRunCar = function(carId) {
+	if ( this.isStopCar ) {
+		callSystemLuaFunction("wayPoints.stopCar", carId);
+	}
+	else {
+		callSystemLuaFunction("wayPoints.runCar", carId);
+	}
+}
+
+WaypointsGUI.prototype.showSelectCarForm = function() {
+	var self = this;
+	var frm = this.selectCarForm;
+	$(frm).empty();
+
+	$('<label>CarId: </label>').appendTo(frm);
+	var carsIdCb = $('<select></select>').appendTo(frm);
+	$('<br>').appendTo(frm);
+	$('<button>Select</button>').appendTo(frm).click(function(){
+		var carId = $(carsIdCb).find('option:selected').val();
+		self.stopRunCar(carId);
+		$(frm).fadeOut('slow');
+	});
+	$('<br>').appendTo(frm);
+	$('<button>Close</button>').appendTo(frm).click(function(){
+		$(frm).fadeOut('slow');
+	});
+
+	// fill data
+	this.fillCarsId(carsIdCb);
+
+	$(frm).fadeIn('slow');
+}
+
 WaypointsGUI.prototype.showLoadForm = function() {
 	var self = this;
 	var frm = this.loadFrm;
@@ -85,6 +118,8 @@ WaypointsGUI.prototype.showSaveForm = function() {
 WaypointsGUI.prototype.initialize = function(){
 	this.loadFrm = $('<div class="wpPopupLoadBox" hidden></div>').appendTo(this.rootElement);
 	this.saveFrm = $('<div class="wpPopupSaveBox" hidden></div>').appendTo(this.rootElement);
+	this.selectCarForm = $('<div class="wpSelectCarForm" hidden></div>').appendTo(this.rootElement);
+	this.isStopCar = false;
 	var self = this;
 
 	$('<button>Start recording path</button>').appendTo(this.rootElement).click(function(){
@@ -111,6 +146,16 @@ WaypointsGUI.prototype.initialize = function(){
 
 	$('<button>Stop all cars</button>').appendTo(this.rootElement).click(function(){
         callSystemLuaFunction("wayPoints.stopAllCars", "");
+    });
+
+	$('<button>Stop car</button>').appendTo(this.rootElement).click(function(){
+		self.isStopCar = true;
+        self.showSelectCarForm();
+    });
+
+	$('<button>Run car</button>').appendTo(this.rootElement).click(function(){
+		self.isStopCar = false;
+        self.showSelectCarForm();
     });
 
 	$('<button>Print current car id</button>').appendTo(this.rootElement).click(function(){
